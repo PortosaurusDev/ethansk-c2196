21c50b6193c03401420ee893c535da4d661e1c3d
PortosaurusBot <claperone.portasaurus@gmail.com>
2020-06-06 11:29:08 +0100

    Auto generated portfolio

---
+++ precache-manifest.95215c0ed0544b73d1c130ad6fc6f74e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de97bfdeee6aeece8d412f4c803e8f7f",
+    "url": "portosaurusdev.github.io/ethansk-c2196/index.html"
+  },
+  {
+    "revision": "9bbc26767616a69162c3",
+    "url": "portosaurusdev.github.io/ethansk-c2196/static/css/2.2c0f828a.chunk.css"
+  },
+  {
+    "revision": "d73f156004ce9a38fcef",
+    "url": "portosaurusdev.github.io/ethansk-c2196/static/css/main.4319e8d8.chunk.css"
+  },
+  {
+    "revision": "9bbc26767616a69162c3",
+    "url": "portosaurusdev.github.io/ethansk-c2196/static/js/2.5b76354d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "portosaurusdev.github.io/ethansk-c2196/static/js/2.5b76354d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73f156004ce9a38fcef",
+    "url": "portosaurusdev.github.io/ethansk-c2196/static/js/main.c268dd06.chunk.js"
+  },
+  {
+    "revision": "1cfbe66095ad0c92bb20",
+    "url": "portosaurusdev.github.io/ethansk-c2196/static/js/runtime-main.3ffd3261.js"
+  },
+  {
+    "revision": "d1055b19fe53d68547369e312be91bac",
+    "url": "portosaurusdev.github.io/ethansk-c2196/static/media/defaultbanner.d1055b19.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "portosaurusdev.github.io/ethansk-c2196/precache-manifest.95215c0ed0544b73d1c130ad6fc6f74e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("portosaurusdev.github.io/ethansk-c2196/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
